5e2cc1eaaa75612bf0e4d8c6684ad9ad6a8a47a6
MatheusMarinelli <matheushlmarinelli@gmail.com>
2023-05-23 22:43:52 -0300

    usando @Payload e criando um deserializador no serviço newcustomerconsumerservice

--- newcustomerconsumerservice/src/main/java/br/com/alura/telefonica/kafka/boost/newcustomerconsumerservice/framework/adapter/in/kafka/CustomerSubscriber.java
+++ newcustomerconsumerservice/src/main/java/br/com/alura/telefonica/kafka/boost/newcustomerconsumerservice/framework/adapter/in/kafka/CustomerSubscriber.java
@@ -1,7 +1,9 @@
 package br.com.alura.telefonica.kafka.boost.newcustomerconsumerservice.framework.adapter.in.kafka;
 
+import br.com.alura.telefonica.kafka.boost.newcustomerconsumerservice.domain.Customer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -9,7 +11,7 @@ import org.springframework.stereotype.Component;
 public class CustomerSubscriber {
 
     @KafkaListener(topics = "${topic.customer}")
-    public void consumeCustomer(String message) {
+    public void consumeCustomer(@Payload Customer message) {
         log.info("MESSAGE CONSUMED {}",message);
     }
 
---
+++ newcustomerconsumerservice/src/main/java/br/com/alura/telefonica/kafka/boost/newcustomerconsumerservice/framework/helper/deserializer/CustomerDeserializer.java
@@ -0,0 +1,20 @@
+package br.com.alura.telefonica.kafka.boost.newcustomerconsumerservice.framework.helper.deserializer;
+
+import br.com.alura.telefonica.kafka.boost.newcustomerconsumerservice.domain.Customer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.io.IOException;
+
+public class CustomerDeserializer<T> implements Deserializer<T> {
+
+
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        try {
+            return (T) new ObjectMapper().readValue(data, Customer.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
