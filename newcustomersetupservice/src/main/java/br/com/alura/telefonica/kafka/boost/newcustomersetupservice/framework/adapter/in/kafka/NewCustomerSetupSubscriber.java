565ca24bcb5f650d126b9ae38c0460d072e3e791
MatheusMarinelli <matheushlmarinelli@gmail.com>
2023-06-01 17:38:40 -0300

    configurando os MSs para usar o deserializador genérico

--- newcustomersetupservice/src/main/java/br/com/alura/telefonica/kafka/boost/newcustomersetupservice/framework/adapter/in/kafka/NewCustomerSetupSubscriber.java
+++ newcustomersetupservice/src/main/java/br/com/alura/telefonica/kafka/boost/newcustomersetupservice/framework/adapter/in/kafka/NewCustomerSetupSubscriber.java
@@ -1,18 +1,17 @@
 package br.com.alura.telefonica.kafka.boost.newcustomersetupservice.framework.adapter.in.kafka;
 
+import br.com.alura.telefonica.kafka.boost.common.domain.Customer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class NewCustomerSetupSubscriber {
-
-
-
     @KafkaListener(topics = "${topic.customer}")
-    public void consumeNewCustomer(String message) {
-      log.info("SCHEDULING NEW INSTALLATION FOR CUSTOMER {}",message);
+    public void consumeNewCustomer(@Payload Customer customer) {
+      log.info("SCHEDULING NEW INSTALLATION FOR CUSTOMER {}",customer);
     }
 
 
